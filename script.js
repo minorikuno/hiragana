105f620014df68f4394ab1fe2f60dca30e1ee36b
MinoriKUNO <minori.fr@gmail.com>
2021-06-10 16:59:50 +0200

    responsived

--- script.js
+++ script.js
@@ -24,10 +24,13 @@ $(function () {
   });
   /* ckick event exemple */
 
-  /* Hand wrting space */
+
+
+  /* Hand wrting sketchpad with mouse */
   let canvas, ctx;
   let mouseX, mouseY, mouseDown = 0;
-
+  var touchX,touchY;
+  
 
   function drawDot(ctx, x, y, size) {
     r = 153; g = 123; b = 173; a = 255;
@@ -82,20 +85,68 @@ $(function () {
 
   }
 
-  // remplace body onload="init()"
-  $(document).ready(function init() {
-    canvas = document.getElementById('sketchpad');
+  
+   /* Hand wrting sketchpad by touching */
 
-    if (canvas.getContext) {
-      ctx = canvas.getContext('2d');
+
+function sketchpad_touchStart(){
+  getTouchPos();
+
+  drawDot(ctx, touchX, touchY, 8);
+
+  event.preventDefault();
+}
+
+function sketchpad_touchMove(e){
+  getTouchPos(e);
+
+  drawDot(ctx, touchX, touchY, 8);
+
+  event.preventDefault();
+}
+
+function getTouchPos(e){
+  
+  if(!e){
+    let e = Event;
+  }
+  
+  if(event.touches){
+    
+    if(event.touches.length == 1){
+      
+      let touch = event.touches[0];
+      touchX = touch.pageX-touch.target.offsetLeft;
+      touchY = touch.pageY-touch.target.offsetTop;
     }
+  }
+}
 
-    if (ctx) {
-      canvas.addEventListener('mousedown', sketchpad_mouseDown, false);
-      canvas.addEventListener('mousemove', sketchpad_mouseMove, false);
-      window.addEventListener('mouseup', sketchpad_mouseUp, false);
 
-    }
-  })
+// remplace body onload="init()"
+$(document).ready(function init() {
+  canvas = document.getElementById('sketchpad');
+ 
+
+  if (canvas.getContext) {
+    ctx = canvas.getContext('2d');
+  }
+
+  if (ctx) {
+    // By mouse
+    canvas.addEventListener('mousedown', sketchpad_mouseDown, false);
+    canvas.addEventListener('mousemove', sketchpad_mouseMove, false);
+    window.addEventListener('mouseup', sketchpad_mouseUp, false);
+
+    // By touching
+    canvas.addEventListener('touchstart', sketchpad_touchStart, false);
+    canvas.addEventListener('touchmove', sketchpad_touchMove, false);
+  }
+});
+
+
+
+
+
 
 });
